686f6378306fcd1e96e396a516d3f4a770259299
Tiago I <tiago.al@gmail.com>
2023-05-31 20:37:43 -0300

    update tela login
    update tela login

--- src/main/java/com/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algafood/core/security/ResourceServerConfig.java
@@ -27,7 +27,7 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.formLogin()
+		.formLogin().loginPage("/entrar")
 		.and()
 		.authorizeRequests()
 			.antMatchers("/oauth/**").authenticated()
--- src/main/java/com/algafood/core/security/authorizationServer/SecurityController.java
+++ src/main/java/com/algafood/core/security/authorizationServer/SecurityController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SecurityController {
 
-//	@GetMapping("/login")
-//	public String login() {
-//		return "pages/login";
-//	}
+	@GetMapping("/entrar")
+	public String login() {
+		return "pages/entrar";
+	}
 	
 }
